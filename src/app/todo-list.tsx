f908ac979fe6bf51c64732620ba8a81e9022340c
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2023-01-18 23:27:51 +0500

    I have added update delete functionalities in my todo-app and solved the (CORS) problem to communicate to API.

--- src/app/todo-list.tsx
+++ src/app/todo-list.tsx
@@ -1,26 +1,27 @@
-interface Todo {
+import Todo from "./todo";
+
+interface TodoObj {
   id: string;
   name: string;
   isDone: boolean;
-}
+};
+
 
 const getTodos = async () => {
-  let todos = await fetch("http://localhost:3001/api/todo/list");
-  return todos.json();
+  let todo = await fetch("http://localhost:3001/api/todo/list");
+  return todo.json();
 };
 
 export default async function TodoList() {
   const { todos } = await getTodos();
   console.log(todos);
-
   return (
     <div>
       <ul style={{ listStyleType: "none", padding: 0 }}>
-        {todos.map((t: Todo) => {
+        {todos.map((i: TodoObj) => {
           return (
-            <li key={t.id} style={{ padding: "5px 0" }}>
-              {t.name}
-              <button style={{ margin: "5px" }}> Delete </button>
+            <li key={i.id} style={{ padding: "5px 0" }}>
+              <Todo todo={i} />
             </li>
           );
         })}
---
+++ src/app/todo.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+async function update(id: string, isDone: boolean, refresh: () => void) {
+  await fetch(`/api/todo/update`, {
+    method: "POST",
+    body: JSON.stringify({ id, isDone }),
+  });
+  refresh();
+}
+
+async function deleteTodo(id: string, refresh: () => void) {
+  await fetch(`/api/todo/delete?id=${id}`, {
+    method: "DELETE",
+  });
+  refresh();
+}
+
+export default function Todo({ todo }: any) {
+  const router = useRouter();
+
+  return (
+    <>
+      <input
+        type="checkbox"
+        onChange={(e) => update(todo.id, e.target.checked, router.refresh)}
+        checked={todo.isDone}
+      />
+      <span>{todo.name}</span>
+      <button onClick={() => deleteTodo(todo.id, router.refresh)}>
+        Delete
+      </button>
+    </>
+  );
+}
